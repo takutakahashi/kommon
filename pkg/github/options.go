90d059865e30089c93fa1840a5bae35a4c31f33f
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2025-02-11 13:59:01 +0900

    fix: update interfaces and fix compile errors
    - Update agent options and interfaces
    - Fix GitHub provider implementation
    - Simplify client helper interface
    - Fix main command implementation

--- pkg/github/options.go
+++ pkg/github/options.go
@@ -4,9 +4,18 @@ package github
 type Options struct {
 	Owner    string
 	Repo     string
-	PRNumber int
-	// GitHub specific options can be added here
-	// APIEndpoint string
-	// IncludeReviewComments bool
-	// etc...
+	Number   int
+	Token    string
+	Type     string // "issue" or "pr"
+}
+
+// NewOptions creates a new Options instance with default values
+func NewOptions(owner, repo string, number int, token string, commentType string) *Options {
+	return &Options{
+		Owner:  owner,
+		Repo:   repo,
+		Number: number,
+		Token:  token,
+		Type:   commentType,
+	}
 }
